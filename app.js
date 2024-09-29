55c597118617a0f27082701d20f2411e6baf20c1
andredw13L <andreribeiro13l@gmail.com>
2024-09-29 11:02:06 -0300

    Iniciar o Servidor da API

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ const app = express()
 
 
 // Importando as rotas da aplicação relacionadas às tarefas
-const tarefasRoutes = require('./tarefaRoutes')
+const tarefaRoutes = require('./tarefaRoutes')
 
 
 // Definindo a porta em que o servidor irá escutar
 const PORT = process.env.PORT || 3000
 
 
+// Configurando uma rota inicial para exibir uma mensagem de boas-vindas
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à API de tarefas!');
+});
+
+
 // Configurando as rotas da aplicação para utilizar as rotas relacionadas às tarefas
-app.use('/api', tarefasRoutes)
+app.use('/api', tarefaRoutes)
 
 
 // Inicializando o servidor e fazendo com que ele escute na porta definida
