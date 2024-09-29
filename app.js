0fe6b583a7c57a9465265cc80e3ea5ffd00998e0
andredw13L <andreribeiro13l@gmail.com>
2024-09-29 10:56:06 -0300

    Configurando a Aplicação Principal

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ const express = require('express')
 const app = express()
 
 
+// Importando as rotas da aplicação relacionadas às tarefas
+const tarefasRoutes = require('./tarefaRoutes')
+
+
 // Definindo a porta em que o servidor irá escutar
 const PORT = process.env.PORT || 3000
 
 
-// Configurando uma rota inicial para exibir uma mensagem de boas-vindas
-app.get('/', (req, res) => {
-    res.send('Bem vindo à API de tarefas!')
-})
-
+// Configurando as rotas da aplicação para utilizar as rotas relacionadas às tarefas
+app.use('/api', tarefasRoutes)
 
 
 // Inicializando o servidor e fazendo com que ele escute na porta definida
